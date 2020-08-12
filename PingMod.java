9d1ae36ad334be80b06cdbbb7bfd503b36edfbdd
HyptexPvP <63811460+HyptexPvP@users.noreply.github.com>
2020-08-12 15:56:20 -0400

    Create PingMod.java

---
+++ PingMod.java
@@ -0,0 +1,1 @@
+this.fr.drawStringWithShadow(this.mc.getCurrentServerData().pingToServer + " ms", (float)(this.pos.getAbsoluteX() + (50 - this.fr.getStringWidth(this.mc.getCurrentServerData().pingToServer + " ms")) / 2), (float)(this.pos.getAbsoluteY() + 3), -1)
