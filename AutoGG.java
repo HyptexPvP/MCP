bf5297a142936e7b097f4860c5f271c2b9f85bd0
HyptexPvP <63811460+HyptexPvP@users.noreply.github.com>
2020-10-20 10:55:07 -0400

    Update AutoGG.java

--- AutoGG.java
+++ AutoGG.java
@@ -2,7 +2,7 @@
 	            S45PacketTitle packet = (S45PacketTitle) e.getPacket();
 	            if (packet.getType().equals(S45PacketTitle.Type.TITLE)) {
 	                String text = packet.getMessage().getUnformattedText();
-	                if (text.contains("胜利") || text.toUpperCase().contains("VICTORY") || text.contains("승리")) {
+	                if (text.toUpperCase().contains("VICTORY")) {
 	                    mc.thePlayer.sendChatMessage("/ac gg ");
 	                }
 	            }
