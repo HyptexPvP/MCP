1de52f7c086d55f1e4500fadef9eac541bcb4c4f
HyptexPvP <63811460+HyptexPvP@users.noreply.github.com>
2020-10-20 10:56:29 -0400

    Update PingMod.java

--- PingMod.java
+++ PingMod.java
@@ -1,1 +1,1 @@
-fr.drawString(this.mc.getCurrentServerData().pingToServer + " ms", (float)(this.pos.getAbsoluteX() + (50 - this.fr.getStringWidth(this.mc.getCurrentServerData().pingToServer + " ms")) / 2), (float)(this.pos.getAbsoluteY() + 3), -1)
+this.mc.getCurrentServerData().pingToServer
